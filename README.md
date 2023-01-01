2eb73de2c89c80afc5d297cd9d7019685f3f93aa
mvonsteinkirch <bt3@protonmail.ch>
2023-01-01 13:45:42 -0800

    clean up scripts

--- README.md
+++ README.md
@@ -1,22 +1,16 @@
 # 💾🌌 "hidden figures" from the metaverse
 
 <br>
-<br>
 
 
-### a quick-and-dirty [steganography](https://en.wikipedia.org/wiki/Steganography) proof-of-concept using images from the Hubble Space Telescope.
+#### a quick-and-dirty [steganography](https://en.wikipedia.org/wiki/Steganography) proof-of-concept using images from the Hubble Space Telescope.
 
-<br>
-
-#### note: i wrote this in 1h and on a terrible text editor - the code is messy and far from being "production" code. if you would like to evaluate my abilities in writing beautiful production code, please check any other repo in this lab. but the math is pretty tho 😇.
-
-<br>
 <br>
 
 
 <p align="center">
 <a href="https://rarible.com/token/0x60f80121c31a0d46b5279700f9df786054aa5ee5:539811?tab=details"><img src="https://user-images.githubusercontent.com/1130416/169350582-92b2811c-1b18-4d80-aa0e-b3b778a428d5.png" width="100%" align="center" style="padding:1px;border:1px solid black;"   title="Can you see the hidden message?" /></a>
-  <br> <a href="https://bt3gl.xyz/hidden-figures-in-the-metaverse">(𝚌𝚊𝚗 𝚢𝚘𝚞 𝚜𝚎𝚎 𝚝𝚑𝚎 𝚑𝚒𝚍𝚍𝚎𝚗 𝚒𝚖𝚊𝚐𝚎?)</a>
+<br> <br> <a href="https://bt3gl.xyz/hidden-figures-in-the-metaverse">(check other metaverse steganography images here)</a>
 </p>
     
 
@@ -24,16 +18,28 @@
 
 ----
 
-## running your own images
+### running with your own images
 
 <br>
 
+install deps with:
 
-1. install `pipenv install`
-2. add background and foreground images to their respective dirs.
-3. edit the constants in `inner.py`
-4. run `inner.py`
+```
+pipenv install
+````
 
-<br>
+add `env` vars:
+
+```
+cp .env.example .env
+vim .env
+```
+
+add background and foreground images to their respective dirs (`background` and `foreground`), and then run:
+
+```
+./src/main.py
+```
+
 <br>
 
---
+++ src/utils.py
@@ -0,0 +1,62 @@
+#######################################################################
+#############           Inner Figures from the Metaverse
+#############                by steinkirch
+#############                   utils.py
+#######################################################################
+
+
+import os
+from pathlib import Path
+from PIL import Image as im
+from dotenv import load_dotenv
+
+
+def load_file(imagefile):
+    """Load an image file and return an image object."""
+    try:
+        return im.open(imagefile)
+    except IOError as e:
+        print('Could not open {0}: {1}'.format(imagefile, e))
+        raise
+
+
+def save_to_file(image, outfile):
+    """Save an image object to a file."""
+    try:
+        image.save(outfile)
+    except IOError as e:
+        return "Could not create file '{}': {}".format(outfile, e)
+
+
+def load_config() -> dict:
+    """Load and set environment variables."""
+
+    env_file = Path('.') / '.env'
+    if not os.path.isfile(env_file):
+        print('Please create an .env file')
+
+    env_vars = {}
+    load_dotenv(env_file)
+
+    try:
+        env_vars['FIGURES'] = os.getenv("FIGURES")
+        env_vars['DEFAULT_FONT'] = os.getenv("DEFAULT_FONT")
+        env_vars['BGFOLDER'] = os.getenv("BGFOLDER")
+        env_vars['FGFOLDER'] = os.getenv("FGFOLDER")
+        env_vars['OUTFOLDER'] = os.getenv("OUTFOLDER")
+        env_vars['BLUR'] = os.getenv("BLUR")
+        env_vars['OVERSAMPLE'] = os.getenv("OVERSAMPLE")
+        env_vars['SHIFT_RATIO'] = os.getenv("SHIFT_RATIO")
+        env_vars['LEFT_TO_RIGHT'] = os.getenv("LEFT_TO_RIGHT")
+        env_vars['DOT_OVER_PATTERN_PROBABILITY'] = os.getenv("DOT_OVER_PATTERN_PROBABILITY")
+        env_vars['WALL'] = os.getenv("WALL")
+        env_vars['FORCE_DEPTH'] = os.getenv("FORCE_DEPTH")
+        env_vars['PATTERN_FRACTION'] = os.getenv("PATTERN_FRACTION")
+        env_vars['CANVAS_SIZE'] = os.getenv("CANVAS_SIZE")
+        env_vars['COLOR_SHOPIFY'] = os.getenv("COLOR_SHOPIFY")
+        env_vars['COLORMAP'] = os.getenv("COLORMAP")
+        
+        return env_vars
+
+    except KeyError as e:
+        print(f'Cannot extract env variables: {e}. Exiting.')
